74e7909f866139ba2ab33b581617bce9e0fa5897
her-mela <90832566+her-mela@users.noreply.github.com>
2022-04-21 00:32:56 +0300

    Add files via upload

---
+++ friends/js.js
@@ -0,0 +1,12 @@
+const api="https://friends-quotes-api.herokuapp.com/quotes/random";
+let actor=document.getElementById('actor');
+let quote=document.getElementById('quote');
+
+function quotegenerator(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        actor.innerHTML=data.character;
+        quote.innerHTML=data.quote;
+          console.log(data);
+    });
+}
+document.getElementById('btn').onclick=function ehh(){quotegenerator(api);}
